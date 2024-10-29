da0ded1055ae9c018d3a33e107b8ac745c2aa6fd
giovanna-clc <giovanna.couto@sempreceub.com>
2024-10-29 10:49:42 -0300

    Atualizações - 29.10

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Olá, mundo!");
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// ROTAS //
+
+app.get('/', (req, res) => {
+   res.send("Chamada ao recurso realizada com sucesso!");
+});
+
+// Retornar todos os usuários
+app.get('/users', (req, res) => {
+    res.send ("Aqui vou retornar todos os usuários do sistema");
+});
+
+app.get('/user/:id', (req, res) => {
+    // Aqui vou logar o valor do parâmetro
+    console.log(req.params.id);
+});
+
+app.post('rotapost', (req, res) => {
+    res.send("Chamada ao recurso usando o post realizado com sucesso!");
+});
+
+app.listen(PORT, () => {
+    console.log("Servidor aguardando requisições.");
+});
