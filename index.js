ca88a56f1f9647b063b98452369735bcd3708b44
giovanna-clc <giovanna.couto@sempreceub.com>
2024-11-26 08:49:28 -0300

    Atualizações dos códigos com base na aula passada

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
+const cors = require ('cors');
 const PORT = 3000;
+
+const sequelize = require('./config/db');
+const Usuario = require('./models/usuario');
+const Ponto = require('./models/ponto');
+
 
 sequelize.sync({ alter: true })
 .then(() => {
@@ -10,26 +16,79 @@ sequelize.sync({ alter: true })
     console.log("Erro!");
 });
 
-// ROTAS //
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-   res.send("Chamada ao recurso realizada com sucesso!");
+
+// Rota que retorna TODOS os usuários da aplicação
+app.get('/usuarios', async (req, res) => {
+    // Como tratar erro (try/catch)?
+    
+    const usuarios = await Usuario.findAll();
+    res.json(usuarios);
 });
 
-// Retornar todos os usuários
-app.get('/users', (req, res) => {
-    res.send ("Aqui vou retornar todos os usuários do sistema");
+
+// Rota que busca um usuário específico 
+app.get('/usuario/:id_usuario', async (req, res) => {
+    const usuario = await Usuario.findAll({
+        where: {
+          id_usuario: req.params.id_usuario
+        },
+    });
+
+    res.json(usuario);
 });
 
-app.get('/user/:id', (req, res) => {
-    // Aqui vou logar o valor do parâmetro
-    console.log(req.params.id);
+
+// Rota que cria um usuário
+app.post('/usuario', async (req, res) => {
+    // Fazer isso em formato de desestruturação
+    // {nome, email ...} = req.body
+    const usuario = await Usuario.create({
+        nome: req.body.nome,
+        email: req.body.email,
+        login: req.body.login,
+        senha: req.body.senha,
+        permissao: req.body.permissao
+    });
+    //https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/201
+    res.status(201).json(usuario);
 });
 
-app.post('rotapost', (req, res) => {
-    res.send("Chamada ao recurso usando o post realizado com sucesso!");
+// Rota para deletar um usuário específico (id)
+app.delete('/usuario/:id_usuario', async (req, res) => {
+    const id_usuario = req.params.id_usuario;
+
+    const usuario = await Usuario.findByPk(id_usuario);
+
+    if(!usuario) {
+       // retornar um erro com status adequado
+       return res.send("Erro ao deletar usuário"); 
+    }
+
+    await usuario.destroy();
+    res.send("Usuário deletado com sucesso");
 });
+
+
+// Editar atributos do usuário id
+app.put('/usuario/:id_usuario', async (req, res) => {
+
+    const id_usuario = req.params.id_usuario;
+    const { nome, email, login, senha, permissao } = req.body;
+
+    const usuario = await Usuario.findByPk(id_usuario);
+
+    if(!usuario) {
+        return res.send("Erro ao editar usuário");
+    }
+
+    usuario.update({ nome, email, login, senha, permissao });
+    res.send("Usuário editado com sucesso");
+});
+
 
 app.listen(PORT, () => {
-    console.log("Servidor aguardando requisições.");
+    console.log("Servidor aguardando requisições");
 });
